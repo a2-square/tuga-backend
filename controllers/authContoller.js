55fbee78a12ff1c73e27c83aa0fb3fb6488360c0
ajit-vinove <ajit.pandit@mail.vinove.com>
2016-10-19 18:18:35 +0530

    Update authContoller.js

--- controllers/authContoller.js
+++ controllers/authContoller.js
@@ -27,42 +27,6 @@ exports.simpleSignup = function(req, res) {
     });
 };
 
-exports.simplelogin = function(req, res, next) {
-    console.log(req.body);
-    if (req.body.email) {
-        User.findOne({ email: req.body.email }, function(err, user) {
-            if (err) {
-                res.json({
-                    authentication: false,
-                    message: "Sorry! Something went wrong, please try to login again"
-                });
-            }
-            if (!user) {
-                res.json({
-                    authentication: false,
-                    message: "Sorry! Incorrect email address"
-                });
-            }
-            if (!user.validPassword(req.body.password)) {
-                res.json({
-                    authentication: false,
-                    message: "Sorry! Incorrect password"
-                });
-            }
-            res.json({
-                authentication: true,
-                message: 'successfully login',
-                user: user
-            });
-        });
-    } else {
-        res.json({
-            authentication: false,
-            message: "Invalid Request"
-        });
-    }
-}
-
 exports.logout = function(req, res) {
 	req.logOut();
 	res.json({
