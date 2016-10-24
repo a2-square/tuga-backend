cf2a88ccf202fbd15001c2f54c6c0f5a4d5c87f2
ajit-vinove <ajit.pandit@mail.vinove.com>
2016-10-24 10:32:29 +0530

    Update passport_config.js

--- config/passport/passport_config.js
+++ config/passport/passport_config.js
@@ -1,12 +1,12 @@
 module.exports = {
   facebook: {
-    clientID: '1120223188025066',
-    clientSecret: 'd04d2bf1b3c31b77d54b13fcd3968262',
+    clientID: 'XXXXXXXXXXXXXX',
+    clientSecret: 'XXXXXXXXXXXXXX',
     callbackURL: 'http://localhost:7000/auth/facebook'
   },
   linkedin: {
-    consumerKey: '815ky16majzilt',
-    consumerSecret: 'b6e39CKsdzZyF0BR',
+    consumerKey: 'XXXXXXXXXXXXXX',
+    consumerSecret: 'XXXXXXXXXXXXXX',
     callbackURL: "http://localhost:7000/auth/linkedin"
   },
 
